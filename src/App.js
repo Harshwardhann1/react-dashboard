2e7c64dac6e0d32ef7916072fe62716e68e03dc1
Harsh Wardhan <wardhan071@gmail.com>
2024-09-30 09:03:34 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { useStateContext } from './contexts/ContextProvider';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import {
@@ -26,7 +27,8 @@ import {
 import './App.css';
 
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { BsChatLeft } from 'react-icons/bs';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { RiNotification3Line } from 'react-icons/ri';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import avatar from '../data/avatar.jpg';
+import { Card, Chat, Notification, UserProfile } from '.';
+import { useStateContext } from '../contexts/ContextProvider';
+import { FiShoppingCart } from 'react-icons/fi';
+
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
+  <TooltipComponent content={title} position='BottomCenter'><button type='button' onClick={customFunc}
+  style={{color}}
+  className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+  >
+    <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'>{icon}</span></button></TooltipComponent>
+};
 
 const Navbar = () => {
+  const { activeMenu, setActiveMenu , handleClick} = useStateContext();
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+       <div className="flex">
+      <NavButton
+        title="title"
+        customFunc={() => handleClick('cart')}
+        color="blue"
+        icon={<FiShoppingCart />}
+      />
+    </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
